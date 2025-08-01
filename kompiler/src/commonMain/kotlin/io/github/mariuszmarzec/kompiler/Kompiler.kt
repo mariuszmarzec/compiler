002860f4f93a93e6d5f873320573dbc92fae7e5a
Mariusz Marzec <mariusz.marzec00@gmail.com>
2025-08-02 00:22:53 +0200

    Added ONP compiler implementation as starting point

--- kompiler/src/commonMain/kotlin/io/github/mariuszmarzec/kompiler/Kompiler.kt
+++ kompiler/src/commonMain/kotlin/io/github/mariuszmarzec/kompiler/Kompiler.kt
@@ -1,4 +1,82 @@
 package io.github.mariuszmarzec.kompiler
 
 class Kompiler {
+
+    fun compile(exp: String): String {
+        val output = mutableListOf<String>()
+        val stack = ArrayDeque<String>()
+        for (ch in exp) {
+            printInput(output)
+            println("ch = $ch")
+            when (ch) {
+                in allowedCharacters() -> {
+                    println("allowedCharacters")
+                    if (output.lastOrNull()?.first() in allowedCharacters()) {
+                        output[output.lastIndex] += ch
+                    } else {
+                        output.add(ch.toString())
+                    }
+                }
+                in openingOperator() -> {
+                    stack.addLast(ch.toString())
+                }
+                in closingOperator() -> {
+                    while (stack.isNotEmpty() && stack.last() != "(") {
+                        output.add(stack.removeLast())
+                    }
+                    if (stack.isNotEmpty() && stack.last() == "(") {
+                        stack.removeLast() // Remove the '('
+                    }
+                    else {
+                        throw IllegalArgumentException("Mismatched parentheses in expression: $exp")
+                    }
+                }
+                ' ' -> {
+                    // Ignore whitespace
+                }
+                in regularOperators() -> {
+                    while (stack.isNotEmpty() && operators()[ch]!! <= operators()[stack.last().first()]!!) {
+                        output.add(stack.removeLast())
+                    }
+                    stack.addLast(ch.toString())
+                }
+                else -> {
+                    throw IllegalArgumentException("Invalid character in expression: $ch")
+                }
+            }
+            println(printInput(output))
+        }
+        while (stack.isNotEmpty()) {
+            output.add(stack.removeLast())
+        }
+        return printInput(output)
+    }
+
+    private fun closingOperator() = listOf(')')
+
+    private fun openingOperator(): List<Char> = listOf('(')
+
+    private fun allowedCharacters(): List<Char> =
+        (('0'..'9') + ('a'..'z') + ('A'..'Z'))
+
+    private fun operators(): Map<Char, Int> = mapOf(
+        '(' to 0,
+        '-' to 0,
+        '+' to 1,
+        ')' to 1,
+        '*' to 2,
+        '/' to 2,
+        '%' to 2,
+        '^' to 3
+    )
+
+    private fun regularOperators(): List<Char> =
+        operators().keys.filter { it !in openingOperator() && it !in closingOperator() }
 }
+
+private fun printInput(input: MutableList<String>): String = input.fold(StringBuilder()) { acc, item ->
+    if (!acc.isEmpty()) {
+        acc.append(" ")
+    }
+    acc.append(item)
+}.toString()
--- kompiler/src/commonTest/kotlin/io/github/mariuszmarzec/kompiler/Kompiler.kt
+++ kompiler/src/commonTest/kotlin/io/github/mariuszmarzec/kompiler/Kompiler.kt
@@ -1,4 +1,16 @@
 package io.github.mariuszmarzec.kompiler
 
+import org.junit.Test
+import kotlin.test.assertEquals
+
 class KompilerTest {
+
+    val kompiler = Kompiler()
+
+    @Test
+    fun onpTest() {
+        assertEquals("2 3 + 5 *", kompiler.compile("(2+3)*5"))
+        assertEquals("2 7 + 3 / 14 3 − 4 * + 2 /", kompiler.compile("((2+7)/3+(14−3)*4)/2"))
+        assertEquals("12 a b c × d e / + × +", kompiler.compile("12 + a × (b × c + d / e)"))
+    }
 }
