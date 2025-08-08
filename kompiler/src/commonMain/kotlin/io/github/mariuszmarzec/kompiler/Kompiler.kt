ff7f471e3a6724d5df66a6cbbd2928260c58c01c
Mariusz Marzec <mariusz.marzec00@gmail.com>
2025-08-08 23:57:20 +0200

    Introduced tokens and handlers

--- kompiler/src/commonMain/kotlin/io/github/mariuszmarzec/kompiler/Kompiler.kt
+++ kompiler/src/commonMain/kotlin/io/github/mariuszmarzec/kompiler/Kompiler.kt
@@ -3,45 +3,26 @@ package io.github.mariuszmarzec.kompiler
 class Kompiler {
 
     fun compile(exp: String): String {
-        val output = mutableListOf<String>()
-        val stack = ArrayDeque<String>()
-        for (ch in exp) {
+        val operatorHandler = OperatorHandler()
+        val literalHandler = LiteralHandler()
+
+        val output = mutableListOf<Token>()
+        val stack = ArrayDeque<Token>()
+        exp.forEachIndexed { index, ch ->
             printInput(output)
             println("ch = $ch")
             when (ch) {
-                in allowedCharacters() -> {
-                    println("allowedCharacters")
-                    if (output.lastOrNull()?.first() in allowedCharacters()) {
-                        output[output.lastIndex] += ch
-                    } else {
-                        output.add(ch.toString())
-                    }
+                in literalHandler.allowedCharacters -> {
+                    literalHandler.handleToken(exp, index, ch, output, stack)
                 }
-                in openingOperator() -> {
-                    stack.addLast(ch.toString())
-                }
-                in closingOperator() -> {
-                    while (stack.isNotEmpty() && stack.last() != "(") {
-                        output.add(stack.removeLast())
-                    }
-                    if (stack.isNotEmpty() && stack.last() == "(") {
-                        stack.removeLast() // Remove the '('
-                    }
-                    else {
-                        throw IllegalArgumentException("Mismatched parentheses in expression: $exp")
-                    }
+                in operatorHandler.allowedCharacters -> {
+                    operatorHandler.handleToken(exp, index, ch, output, stack)
                 }
                 ' ' -> {
-                    // Ignore whitespace
-                }
-                in regularOperators() -> {
-                    while (stack.isNotEmpty() && operators()[ch]!! <= operators()[stack.last().first()]!!) {
-                        output.add(stack.removeLast())
-                    }
-                    stack.addLast(ch.toString())
+                    // TODO handle whitespace as allowed after last one or not allowed
                 }
                 else -> {
-                    throw IllegalArgumentException("Invalid character in expression: $ch")
+                    throw IllegalArgumentException("Invalid character: `$ch` at index $index in expression: $exp")
                 }
             }
             println(printInput(output))
@@ -52,31 +33,118 @@ class Kompiler {
         return printInput(output)
     }
 
-    private fun closingOperator() = listOf(')')
+    private fun allowedCharacters(): List<String> =
+        (('0'..'9') + ('a'..'z') + ('A'..'Z')).map { it.toString() }
 
-    private fun openingOperator(): List<Char> = listOf('(')
-
-    private fun allowedCharacters(): List<Char> =
-        (('0'..'9') + ('a'..'z') + ('A'..'Z'))
-
-    private fun operators(): Map<Char, Int> = mapOf(
-        '(' to 0,
-        '-' to 0,
-        '+' to 1,
-        ')' to 1,
-        '*' to 2,
-        '/' to 2,
-        '%' to 2,
-        '^' to 3
-    )
-
-    private fun regularOperators(): List<Char> =
-        operators().keys.filter { it !in openingOperator() && it !in closingOperator() }
 }
 
-private fun printInput(input: MutableList<String>): String = input.fold(StringBuilder()) { acc, item ->
+private fun printInput(input: MutableList<Token>): String = input.fold(StringBuilder()) { acc, item ->
     if (!acc.isEmpty()) {
         acc.append(" ")
     }
-    acc.append(item)
+    acc.append(item.value)
 }.toString()
+
+interface Token {
+
+    val index: Int
+
+    val value: String
+
+    val opened: Boolean
+}
+
+interface TokenHandler {
+
+    val allowedCharacters: List<Char>
+
+    fun handleToken(
+        exp: String,
+        index: Int,
+        ch: Char,
+        output: MutableList<Token>,
+        stack: ArrayDeque<Token>
+    )
+}
+
+class OperatorHandler : TokenHandler {
+    override val allowedCharacters: List<Char>
+        get() = listOf('(', ')', '+', '-', '*', '/', '%', '^')
+
+    override fun handleToken(
+        exp: String,
+        index: Int,
+        ch: Char,
+        output: MutableList<Token>,
+        stack: ArrayDeque<Token>
+    ) {
+        when(ch.toString()) {
+            in openingOperator() -> {
+                stack.addLast(Operator(index, ch.toString(), opened = false))
+            }
+            in closingOperator() -> {
+                while (stack.isNotEmpty() && stack.last().value != "(") {
+                    output.add(stack.removeLast())
+                }
+                if (stack.isNotEmpty() && stack.last().value == "(") {
+                    stack.removeLast() // Remove the '('
+                }
+                else {
+                    throw IllegalArgumentException("Mismatched parentheses in expression: $exp")
+                }
+            }
+            in regularOperators() -> {
+                while (stack.isNotEmpty() && operators()["$ch"]!! <= operators()[stack.last().value]!!) {
+                    output.add(stack.removeLast())
+                }
+                stack.addLast(Operator(index, ch.toString(), opened = false))
+            }
+            else -> {
+                throw IllegalArgumentException("Lacking handling for operator: $ch at index $index in expression: $exp")
+            }
+        }
+    }
+
+    private fun closingOperator() = listOf(")")
+
+    private fun openingOperator() = listOf("(")
+
+    fun operators(): Map<String, Int> = mapOf(
+        "(" to 0,
+        "-" to 0,
+        "+" to 1,
+        ")" to 1,
+        "*" to 2,
+        "/" to 2,
+        "%" to 2,
+        "^" to 3
+    )
+
+    private fun regularOperators(): List<String> =
+        operators().keys.filter { it !in openingOperator() && it !in closingOperator() }
+}
+
+class LiteralHandler : TokenHandler {
+    override val allowedCharacters: List<Char>
+        get() = ('0'..'9') + ('a'..'z') + ('A'..'Z')
+
+    override fun handleToken(
+        exp: String,
+        index: Int,
+        ch: Char,
+        output: MutableList<Token>,
+        stack: ArrayDeque<Token>
+    ) {
+        println("allowedCharacters")
+        val last = output.lastOrNull()
+        if (last is Literal) {
+            output[output.lastIndex] = last.copy(value = last.value + ch)
+        } else {
+            output.add(Literal(index, ch.toString(), opened = false))
+        }
+    }
+}
+
+data class Operator(override val index: Int, override val value: String, override val opened: Boolean) : Token
+
+data class Literal(override val index: Int, override val value: String, override val opened: Boolean) : Token
--- kompiler/src/commonTest/kotlin/io/github/mariuszmarzec/kompiler/Kompiler.kt
+++ kompiler/src/commonTest/kotlin/io/github/mariuszmarzec/kompiler/Kompiler.kt
@@ -9,6 +9,7 @@ class KompilerTest {
 
     @Test
     fun onpTest() {
+        assertEquals("first second +", kompiler.compile("first + second"))
         assertEquals("2 3 + 5 *", kompiler.compile("(2+3)*5"))
         assertEquals("2 7 + 3 / 14 3 − 4 * + 2 /", kompiler.compile("((2+7)/3+(14−3)*4)/2"))
         assertEquals("12 a b c × d e / + × +", kompiler.compile("12 + a × (b × c + d / e)"))
